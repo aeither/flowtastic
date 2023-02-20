da63bbbc1456854167db672ca8e4f80dcb5dec1e
aeither <36173828+aeither@users.noreply.github.com>
2023-02-20 21:09:07 +0100

    add link portfolio

--- components/layout/user-dropdown.tsx
+++ components/layout/user-dropdown.tsx
@@ -1,12 +1,13 @@
 import {
   Avatar,
-  Button,
+  Link,
   Menu,
   MenuButton,
   MenuItem,
   MenuList,
 } from '@chakra-ui/react'
 import { signOut, useSession } from 'next-auth/react'
+import NextLink from 'next/link'
 import { FC } from 'react'
 
 export const UserDropdown: FC = () => {
@@ -22,6 +23,13 @@ export const UserDropdown: FC = () => {
           <Avatar size={'sm'} name={name || 'acme'} src={image || ''} />
         </MenuButton>
         <MenuList>
+          <Link
+            _hover={{ textDecoration: 'none' }}
+            as={NextLink}
+            href="/portfolio"
+          >
+            <MenuItem>Portfolio</MenuItem>
+          </Link>
           <MenuItem onClick={() => void signOut({ redirect: false })}>
             Logout
           </MenuItem>
