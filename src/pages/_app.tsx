9a96437e31fd18c53723aa25494d2196205d6ad3
aeither <36173828+aeither@users.noreply.github.com>
2023-02-20 12:37:58 +0100

    add chakra-ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
-import { type AppType } from "next/app";
-import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
+import { type AppType } from 'next/app'
+import { type Session } from 'next-auth'
+import { SessionProvider } from 'next-auth/react'
+import { ChakraProvider } from '@chakra-ui/react'
 
-import { api } from "@/utils/api";
+import { api } from '@/utils/api'
 
-import "@/styles/globals.css";
+import '@/styles/globals.css'
+import { theme } from '@/styles/theme'
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -12,9 +14,11 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </SessionProvider>
-  );
-};
+  )
+}
 
-export default api.withTRPC(MyApp);
+export default api.withTRPC(MyApp)
