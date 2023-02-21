a3c636d78bc789506603938388a1c33ad9e87f29
aeither <36173828+aeither@users.noreply.github.com>
2023-02-21 20:50:01 +0100

    rm eslint ts

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,33 +1,34 @@
+// /** @type {import("eslint").Linter.Config} */
+// module.exports = {
+//   overrides: [
+//     {
+//       extends: [
+//         'plugin:@typescript-eslint/recommended-requiring-type-checking',
+//       ],
+//       files: ['*.ts', '*.tsx'],
+//       parserOptions: {
+//         project: 'tsconfig.json',
+//       },
+//     },
+//   ],
+//   parser: '@typescript-eslint/parser',
+//   parserOptions: {
+//     project: './tsconfig.json',
+//   },
+//   plugins: ['@typescript-eslint'],
+//   extends: ['next/core-web-vitals', 'plugin:@typescript-eslint/recommended'],
+//   rules: {
+//     '@typescript-eslint/consistent-type-imports': [
+//       'warn',
+//       {
+//         prefer: 'type-imports',
+//         fixStyle: 'inline-type-imports',
+//       },
+//     ],
+//   },
+// }
+
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  overrides: [
-    {
-      extends: [
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
-      ],
-      files: ['*.ts', '*.tsx'],
-      parserOptions: {
-        project: 'tsconfig.json',
-      },
-    },
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: './tsconfig.json',
-  },
-  plugins: ['@typescript-eslint'],
-  extends: ['next/core-web-vitals', 'plugin:@typescript-eslint/recommended'],
-  rules: {
-    '@typescript-eslint/consistent-type-imports': [
-      'warn',
-      {
-        prefer: 'type-imports',
-        fixStyle: 'inline-type-imports',
-      },
-    ],
-  },
+  extends: 'next/core-web-vitals',
 }
-
-// {
-//   "extends": "next/core-web-vitals"
-// }
