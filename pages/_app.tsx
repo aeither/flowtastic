63d3828dcb33724fb1e7d7ca50787fed2a702cf1
aeither <36173828+aeither@users.noreply.github.com>
2023-02-25 16:56:12 +0100

    update config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,6 @@ import { type Session } from 'next-auth'
 import { SessionProvider } from 'next-auth/react'
 import { type AppType } from 'next/app'
 import { useRouter } from 'next/router'
-import { useEffect } from 'react'
 import { Toaster } from 'react-hot-toast'
 import 'swiper/css'
 import 'swiper/css/navigation'
@@ -18,9 +17,13 @@ import 'swiper/css/pagination'
 const client = createClient({
   fclConfig: {
     ...networks.mainnet,
+    'app.detail.icon':
+      'https://user-images.githubusercontent.com/36173828/221360584-d1cc8df1-f7ad-4fb8-b29b-cc05966eca74.png',
+    'app.detail.title': 'Flowtastic',
     '0xNONFUNGIBLETOKENADDRESS': '0x1d7e57aa55817448',
     '0xGOLAZOSADDRESS': '0x87ca73a41bb50ad5',
     '0xMETADATAVIEWSADDRESS': '0x1d7e57aa55817448',
+    '0xFLOWTASTIC': '0x574277684a825973',
   },
 })
 
