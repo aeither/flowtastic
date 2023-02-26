43c80969489ec404d407e8b0a365d98699e0a659
aeither <36173828+aeither@users.noreply.github.com>
2023-02-26 16:45:09 +0100

    rm pwa theme color

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,5 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
- 
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
 class MyDocument extends Document {
   render() {
     return (
@@ -7,15 +7,14 @@ class MyDocument extends Document {
         <Head>
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon-192x192.png" />
-          <meta name="theme-color" content="#042940" />
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    );
+    )
   }
 }
- 
-export default MyDocument;
+
+export default MyDocument
